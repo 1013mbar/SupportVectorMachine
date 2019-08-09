a00f6c0176d6a8d8a065626b8f3eb0eef34071ef
1013mbar <lucas.kluge@web.de>
2019-08-09 09:08:12 +0200

    Update SVM_algo.py

--- SVM_algo.py
+++ SVM_algo.py
@@ -43,7 +43,36 @@ class Support_Vector_Machine:
             for step in step_sizes:
                 w = np.array([latest_optimum,latest_optimum])
                 optimized = False
+                
                 while not optimized:
+                    for b in np.arange(-1*(self.max_feature_value*b_range_multiple),
+                                       self.max_feature_value*b_range_multiple,
+                                       step*b_multiple):
+                       for transformation in transforms:
+                           w_t = w*transformation
+                           found_option = True
+                           for i in self.data:
+                                for xi in self.data[i]:
+                                    yi = i
+                                    if not yi*(np.dot(w_t,xi)+b) >= 1:
+                                        found_option =False
+                            if found_option:
+                                opt_dict[np.linalg.norm(w_t)]=[w_t,b]
+                                
+                                
+                    if w[0] < 0:
+                        optimized = True
+                        print('Optimized a step')
+                    else:
+                        w = w-step
+                        
+                norms = sorted([n for n in opt_dict])
+            
+                opt_choice = opt_dict[norm[0]]
+                self.w = opt_choice[0]
+                self.b = opt_choice[1]
+                latest_optimum = opt.choice[0][0] + step
+                        
                     
                 
             
